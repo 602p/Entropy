094b362868ae81b4010bd37d48aae0dc2c1052f9
Louis Goessling <louis@goessling.com>
2014-12-05 23:08:18 -0600

    Revert to standard stock_chutes behavior

--- Entropy/Entropy/FailureModules/Parachutes.cs
+++ Entropy/Entropy/FailureModules/Parachutes.cs
@@ -49,15 +49,20 @@ namespace coffeeman
 
 		protected override void DI_Disable()
 		{
-			chute.fullyDeployedDrag /= 2;
-			chute.semiDeployedDrag /= 2;
+			;
 		}
 
 
 		protected override void DI_EvaRepair()
 		{ 
-			chute.fullyDeployedDrag *= 2;
-			chute.semiDeployedDrag *= 2;
+			;
+		}
+
+		protected override void DI_Update(){
+			if (this.HasFailed) {
+				chute.deploymentState = ModuleParachute.deploymentStates.SEMIDEPLOYED;
+				chute.Deploy ();
+			}
 		}
 	}
 }
