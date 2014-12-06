a73644370c813c041460ff87760adb6269588b4f
Louis Goessling <louis@goessling.com>
2014-12-06 12:25:44 -0600

    Reorganize; Add Animation failures

---
+++ Entropy/Entropy/FailureModules/Animate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ippo;
+using UnityEngine;
+using KSP;
+
+namespace coffeeman
+{
+	public class ModuleAnimationReliability : ippo.FailureModule
+	{
+		ModuleAnimateGeneric bay;
+
+		public override string DebugName { get { return "ModuleAnimateGeneric"; } }
+		public override string ScreenName { get { return "Motor"; } }
+		public override string FailureMessage { get { return "A motor has failed"; } }
+		public override string RepairMessage { get { return "You have repaired the motor"; } }
+		public override string FailGuiName { get { return "Fail motor"; } }
+		public override string EvaRepairGuiName { get { return "Repair motor"; } }
+		public override string MaintenanceString { get { return "Lubricate motor"; } }
+
+		public override bool PartIsActive()
+		{
+			// It's only active if its not on the ground
+			return !part.vessel.LandedOrSplashed;
+		}
+
+		protected override void DI_Start(StartState state)
+		{
+			bay = this.part.Modules.OfType<ModuleAnimateGeneric>().Single();
+		}
+
+		protected override bool DI_FailBegin()
+		{
+			return true;
+		}
+
+		protected override void DI_Disable()
+		{
+			bay.allowManualControl = false;
+		}
+
+
+		protected override void DI_EvaRepair()
+		{
+			bay.allowManualControl = true;          
+		}
+	}
+}
+
